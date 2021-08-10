9d38b7b5afc0004c5f3f1fb8fbb4e2cf44f6beca
Sérgio <sehguimaraes@gmail.com>
2021-08-10 17:25:04 -0300

    exercicio resolvido

---
+++ snapcrackle.js
@@ -0,0 +1,21 @@
+function snapCrackle(maxValue) {
+    let text = "";
+    for (let cont = 1; cont <= maxValue; cont++) {
+        if (cont % 2 !== 0) {
+            text += "Snap, ";
+            continue;
+        }
+        if (cont % 5 === 0) {
+        text += "Crackle, ";
+        continue;
+        }
+        if (cont % 2 !== 0 && cont % 5 === 0) {
+            text += "SnapCrackle, ";
+            continue;
+        }
+        text += cont + ", ";
+    }
+    console.log(text);
+}
+
+snapCrackle();
